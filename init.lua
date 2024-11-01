1611e8bcc4648ad1f4ebacd8546d73ce42576821
kt programs <ktprograms@gmail.com>
2024-11-02 01:22:59 +0800

    lsp: use C-s to accept completion

--- init.lua
+++ init.lua
@@ -859,7 +859,7 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<C-s>'] = cmp.mapping.confirm { select = true, behavior = cmp.ConfirmBehavior.Replace },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
