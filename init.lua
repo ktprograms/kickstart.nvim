ce95b553e7cde410ed8b0c08914a3216de4030ee
kt programs <ktprograms@gmail.com>
2024-11-02 01:33:16 +0800

    plug(lastplace): add

--- init.lua
+++ init.lua
@@ -1022,6 +1022,8 @@ require('lazy').setup({
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 
+  { 'ethanholz/nvim-lastplace', opts = {} },
+
 
   {
     'ThePrimeagen/harpoon',
