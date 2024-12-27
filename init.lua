b5401f139a857fdccc5a1c0a68e1516caa04cd47
kt programs <ktprograms@gmail.com>
2024-12-27 13:18:36 +0800

    plug(vim-abolish): add

--- init.lua
+++ init.lua
@@ -1026,6 +1026,8 @@ require('lazy').setup({
     dependencies = 'nvim-treesitter/nvim-treesitter',
   },
 
+  'tpope/vim-abolish',
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
