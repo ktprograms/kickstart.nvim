8024907f7a2748f5e2dfcd7cea6bc4374c33a7dc
kt programs <ktprograms@gmail.com>
2025-01-03 13:25:44 +0800

    colorscheme: change {Comment,LineNr} colour

--- init.lua
+++ init.lua
@@ -799,12 +799,18 @@ require('lazy').setup({
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'EdenEast/nightfox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = {
+      palettes = {
+        all = {
+          comment = '#b382ef', -- rebeccapurple as OKLCH 70% lightness
+        },
+      },
+    },
     init = function()
       -- Load the colorscheme here.
       vim.cmd.colorscheme 'duskfox'
 
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      vim.cmd.hi 'LineNr guifg=#b382ef'
     end,
   },
 
