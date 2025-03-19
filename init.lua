942b14d2e63c761e9b4156ba6704416c56008270
kt programs <ktprograms@gmail.com>
2025-03-19 12:08:05 +0800

    plug(vim-wordmotion): add

--- init.lua
+++ init.lua
@@ -1105,6 +1105,8 @@ require('lazy').setup({
 
   'tpope/vim-abolish',
 
+  'chaoren/vim-wordmotion',
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
