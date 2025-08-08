6cec446ecabe42fad29b95f24bc970584c63abb1
kt programs <ktprograms@gmail.com>
2025-08-08 14:09:08 +0800

    plug(blink.cmp): prioritize exact match
    Fix: templ autocomplete snippets before closing tag

--- init.lua
+++ init.lua
@@ -877,7 +877,10 @@ require('lazy').setup({
       -- the rust implementation via `'prefer_rust_with_warning'`
       --
       -- See :h blink-cmp-config-fuzzy for more information
-      fuzzy = { implementation = 'lua' },
+      fuzzy = {
+        implementation = 'lua',
+        sorts = { 'exact', 'score', 'sort_text' },
+      },
 
       -- Shows a signature help window while you type arguments for a function
       signature = { enabled = true },
